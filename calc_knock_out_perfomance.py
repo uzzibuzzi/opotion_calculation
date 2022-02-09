bd0bbffaf4d12094636b3696184fec4745707ae8
uzzibuzzi <achim_vollmer@gmx.net>
2022-02-09 09:57:06 +0100

    win rate fix

--- calc_knock_out_perfomance.py
+++ calc_knock_out_perfomance.py
@@ -4,19 +4,20 @@ Created on Mon Jul 26 11:43:20 2021
 
 @author: vollmera
 """
-StockPrice=18.04
-target_value=18.9
+StockPrice=155.2
+target_value=170
 
 
 
-KnockOut=13.15
+KnockOut=StockPrice*0.65                #meist so 30 % diff
+    
 
 
 
 
 
 
-lossValue=0.6
+lossValue=0.8               # stop loss limit ca 40%
 
 
 optionPrice=(StockPrice-KnockOut)*0.1
@@ -36,8 +37,13 @@ Winpercent=100*(earning/optionPrice)
 
 print("prize for option {:.2f} first re order {:.2f}  second reorder {:.2f} ".format(optionPrice,optionPrice*0.95,optionPrice*0.9))
 
-print("limits to sell on option is {} underlying value is {}".format(optionPrice*lossValue,StockLimit))
-riskReword=Winpercent/((optionPrice-optionPrice*lossValue)/optionPrice)
+print("limits to sell on option is {:.2f} underlying value is {:.2f}".format(optionPrice*lossValue,StockLimit))
+riskReword=Winpercent/(((optionPrice-optionPrice*lossValue)/optionPrice)*100)
 print("win  {:.1f}%  and risk {:.1f}% reward at {:.1f}".format(Winpercent,100*(1-(lossValue)),riskReword))
 
 # hier mus nioch richtig gerechent werden was ist riskio und chance möglihckeit
+
+
+22/20
+
+Winpercent/(((optionPrice-optionPrice*lossValue)/optionPrice)*100)
